933594d3012e8a4965dbecaa3620aa198b4b15cd
jiafei <jiafei@infinit.com.sg>
2022-03-12 22:47:16 +0800

    init

---
+++ src/test/java/autotest/LoginViewAndroidTest.java
@@ -0,0 +1,83 @@
+package autotest;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginViewAndroidTest {
+
+  private AndroidDriver driver;
+
+  @Before
+  public void setUp() throws MalformedURLException {
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    desiredCapabilities.setCapability("platformName", "Android");
+    desiredCapabilities.setCapability("platformVersion", "9");
+    desiredCapabilities.setCapability("deviceName", "emulator");
+    desiredCapabilities.setCapability("automationName", "UiAutomator2");
+
+    //desiredCapabilities.setCapability("automationName", "uiautomator2");
+    desiredCapabilities.setCapability("appPackage", "com.example.logindemo");
+    desiredCapabilities.setCapability("appActivity", "com.example.logindemo.MainActivity");
+    desiredCapabilities.setCapability("autoGrantPermissions", "true");
+
+    URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+    driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+
+    System.out.println("driver created successfully.");
+  }
+
+  @Test
+  public void loginFailTest() {
+	  
+	//System.out.println(driver.getPageSource());
+	MobileElement el9 = (MobileElement) driver.findElementById("com.example.logindemo:id/edit_username");
+	el9.clear();
+	el9.sendKeys("admin123");
+    MobileElement el10 = (MobileElement) driver.findElementById("com.example.logindemo:id/edit_password");
+    el10.clear();
+    el10.sendKeys("111111");
+    MobileElement el11 = (MobileElement) driver.findElementById("com.example.logindemo:id/buttonLogin");
+    el11.click();
+    
+    WebElement errMsg =  driver.findElement(MobileBy.id("com.example.logindemo:id/err_msg"));
+    System.out.println("errMsg: " + errMsg.getText());
+    Assert.assertEquals("User name or password is not correct",errMsg.getText());
+  }
+  
+  @Test
+  public void loginSuccessTest() {
+
+//System.out.println(driver.getPageSource());
+    MobileElement el9 = (MobileElement) driver.findElementById("com.example.logindemo:id/edit_username");
+    el9.clear();
+    el9.sendKeys("admin");
+    MobileElement el10 = (MobileElement) driver.findElementById("com.example.logindemo:id/edit_password");
+    el10.clear();
+    el10.sendKeys("111111");
+    MobileElement el11 = (MobileElement) driver.findElementById("com.example.logindemo:id/buttonLogin");
+    el11.click();
+
+    WebElement errMsg =  driver.findElement(MobileBy.id("com.example.logindemo:id/err_msg"));
+    System.out.println("errMsg: " + errMsg.getText());
+
+    Assert.assertEquals("Login ok",errMsg.getText());
+    
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+}
---
+++ src/test/java/autotest/LoginViewIOSTest.java
@@ -0,0 +1,81 @@
+package autotest;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginViewIOSTest {
+
+  private static IOSDriver driver;
+  
+
+  /**
+   * 整个测试类只跑一次
+   * */
+  @BeforeClass
+  public static void setUp() throws MalformedURLException {
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    desiredCapabilities.setCapability("platformName", "iOS");
+    desiredCapabilities.setCapability("platformVersion", "15.2");
+    desiredCapabilities.setCapability("deviceName", "iPhone 13 Pro Max");
+    desiredCapabilities.setCapability("automationName", "XCUITest");
+
+    URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+    driver = new IOSDriver(remoteUrl, desiredCapabilities);
+  }
+
+  @Test
+  public void loginFailTest() {
+	  
+	//System.out.println(driver.getPageSource());
+	MobileElement el9 = (MobileElement) driver.findElementByAccessibilityId("username");
+	el9.clear();
+	el9.sendKeys("admin123");
+    MobileElement el10 = (MobileElement) driver.findElementByAccessibilityId("password");
+    el10.clear();
+    el10.sendKeys("111111");
+    MobileElement el11 = (MobileElement) driver.findElementByAccessibilityId("Login");
+    el11.click();
+    
+    WebElement errMsg =  driver.findElement(MobileBy.AccessibilityId("errMsg"));
+    System.out.println("Message: " + errMsg.getText());
+    Assert.assertEquals("Username or password is not correct.", errMsg.getText());
+    
+  }
+  
+  @Test
+  public void loginSuccessTest() {
+	  
+	//System.out.println(driver.getPageSource());
+	MobileElement el9 = (MobileElement) driver.findElementByAccessibilityId("username");
+	el9.clear();
+	el9.sendKeys("admin");
+    MobileElement el10 = (MobileElement) driver.findElementByAccessibilityId("password");
+    el10.clear();
+    el10.sendKeys("111111");
+    MobileElement el11 = (MobileElement) driver.findElementByAccessibilityId("Login");
+    el11.click();
+    
+    WebElement errMsg =  driver.findElement(MobileBy.AccessibilityId("errMsg"));
+    System.out.println("Message: " + errMsg.getText());
+    Assert.assertEquals("Login ok", errMsg.getText());
+  }
+
+  /**
+   * 整个测试类只跑一次
+   * */
+  @AfterClass
+  public static void tearDown() {
+    driver.quit();
+  }
+}
